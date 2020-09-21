1ee1dcbcf7f1b21956311286b50f6acaf7b638f2
Tristan <tristan15thompson@gmail.com>
2020-09-20 22:08:01 -0700

    styled items displayed using css grid

--- mood-shop/resources/js/scripts.js
+++ mood-shop/resources/js/scripts.js
@@ -1,35 +1,34 @@
-import data from './data.js'
+// eslint-disable-next-line import/extensions
+import data from './data.js';
 
-const itemsContainer = document.getElementById('items')
+const itemsContainer = document.getElementById('items');
 
-data.forEach(function (item,i)  {
+data.forEach((item) => {
+  const newDiv = document.createElement('div');
+  newDiv.className = 'item';
 
-    let newDiv = document.createElement('div');
-    newDiv.className = 'item'
+  const img = document.createElement('img');
+  img.src = item.image;
+  img.width = 300;
+  img.height = 300;
 
-    let img = document.createElement('img');
-    img.src = item.image
-    img.width = 300
-    img.height = 300
+  // Add the image to the div
+  newDiv.appendChild(img);
 
-    // Add the image to the div
-    newDiv.appendChild(img)
+  const desc = document.createElement('p');
+  desc.innerText = item.desc;
+  newDiv.appendChild(desc);
 
-    let desc = document.createElement('p')
-    desc.innerText = item.desc
-    newDiv.appendChild(desc)
+  const price = document.createElement('p');
+  price.innerText = item.price;
+  newDiv.appendChild(price);
 
-    let price = document.createElement('p')
-    price.innerText = item.price
-    newDiv.appendChild(price)
+  // creates buy button with a custom attribute data-price
+  const button = document.createElement('button');
+  button.id = item.name;
+  button.dataset.pice = item.price;
+  button.innerHTML = 'Add to Cart';
+  newDiv.appendChild(button);
 
-    // creates buy button with a custom attribute data-price
-    let button = document.createElement('button')
-    button.id = item.name
-    button.dataset.pice = item.price
-    button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
-
-    itemsContainer.appendChild(newDiv) //puts newly created div inside the items container
-
+  itemsContainer.appendChild(newDiv); // puts newly created div inside the items container
 });

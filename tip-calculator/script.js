d8df99fb34c4d5bb1a48ff5991bc4b13647382fd
Tristan <tristan15thompson@gmail.com>
2020-09-22 17:57:02 -0700

    refactored for readability with comments

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,37 +1,20 @@
 const billInput = document.querySelector('#input-bill');
 const tipInput = document.querySelector('#input-tip');
 const peopleInput = document.querySelector('#input-people');
-const tipDisplay = document.querySelector('#display-tip');
-const totalDisplay = document.querySelector('#display-total');
+const displayTip = document.querySelector('#display-tip');
+const displayTotal = document.querySelector('#display-total');
 
-let tipAmount = 0;
-
-function calculateTotal(billValue, peopleValue) {
-  let totalAmount = 0;
-  if (peopleValue < 1) {
-    totalAmount = parseFloat(billValue) + parseFloat(tipAmount);
-  } else if (peopleValue > 0) {
-    totalAmount = (parseFloat(billValue) + parseFloat(tipAmount)) / peopleValue;
-    console.log(totalAmount)
-  }
-
-  totalDisplay.innerHTML = totalAmount;
-}
-
+// will calculate the tip and total amounts to be paid by each person
 function calculateTip() {
-  const billValue = billInput.value;
-  const tipValue = tipInput.value;
-  const peopleValue = peopleInput.value;
+  const billValue = parseFloat(billInput.value);
+  const tipValue = parseFloat(tipInput.value);
+  const peopleValue = parseFloat(peopleInput.value);
 
-  console.log(peopleValue);
-  if (peopleValue < 1) {
-    tipAmount = (billValue * tipValue / 100);
-  } else if (peopleValue > 0) {
-    tipAmount = ((billValue * tipValue / 100) / peopleValue);
-  }
+  const tipAmount = billValue * tipValue / 100 / peopleValue;
+  const total = (billValue + tipAmount) / peopleValue;
 
-  tipDisplay.innerHTML = tipAmount.toFixed(2);
-  calculateTotal(billValue, peopleValue);
+  displayTip.innerHTML = tipAmount.toFixed(2);
+  displayTotal.innerHTML = total.toFixed(2);
 }
 
 billInput.addEventListener('input', calculateTip);
